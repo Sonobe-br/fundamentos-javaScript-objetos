596ad24c94e0c321bc4bc27c1945759d947c20a2
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-08-17 18:45:40 -0300

    Manipulando Objetos no JavaScript

---
+++ objeto.js
@@ -0,0 +1,11 @@
+//arrays
+const listaCPFs = ['27686974863','28081816801','29065912021']
+const cliente = ['nome','Daniel','idade',43]
+
+//modelo de objeto
+const dadosDocliente = {
+    nome:'Daniel',
+    idade: 43,
+    cpf:'27686974863',
+    email:'dev.obe78@gmail.com'
+} 
