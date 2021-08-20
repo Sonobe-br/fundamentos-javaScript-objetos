adadf459fdec58aa69c46ac857dd52ac169656ef
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-08-20 20:10:06 -0300

    Atividades Mack

---
+++ atividade-mack.js
@@ -0,0 +1,40 @@
+const listaDePares = [2,4,6,8,10]
+const digitoDoUsuario1 = 3;
+const digitoDoUsuario2 = 5;
+
+let numeroDigitado = 30;
+let divisor = (numeroDigitado/digitoDoUsuario1/digitoDoUsuario2);
+let operacao = (divisor);
+console.log(operacao)
+
+    if (operacao === listaDePares[2]) {
+        console.log ('O número é divisível por 3 e 5')    
+    } else {
+        console.log ('O número não é divisível por 3 e 5')    
+    }
+
+ 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+//}
+
+
+
+
+
